a99cc404056035a8db55330c478d1ad26fde31ac
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2017-04-10 14:23:04 +0900

    confirm the program

--- statistics/src/sample3.py
+++ statistics/src/sample3.py
@@ -6,56 +6,62 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-def main(df, label):
-    #print "one-way anova F value: " + TODO + "\n degree of freedom between group: " + TODO + "\n degree of freedom within group: " + TODO     
-    
-    # calculate sample_data_average [ave_x, ave_y]
-    within_average = df[label].mean()
-    # print group_average['Japanese']
+# within: gun-nai
+# between: gun-kan
 
-    # TODO calculate total average
-    between_average = np.mean(df[label].values.flatten())
+def main(data, label):
+    ### calculate total average ###
+    within_average = np.mean(df[label].values.flatten())
+    between_average = df[label].mean()
+    # print within_average
     # print between_average
 
-    within_variance = df[label].var(ddof=False)
-    # print group_variance['Japanese']
-
-    # TODO calculate total variance
-    between_variance = np.var(df[label].values.flatten())
+    ### calculate total variance ###
+    within_variance = np.var(df[label].values.flatten())
+    between_variance = df[label].var(ddof=False)
+    # print within_variance
     # print between_variance
     
-    # TODO calculate heihouwa
+    ### calculate heihouwa ###
+    between_heihouwa = 0.0
     within_heihouwa = 0.0
     for i in range(len(label)):
-        within_heihouwa += (between_average - within_average[label[i]]) * (between_average - within_average[label[i]]) * len(df[label[i]])
+        between_heihouwa += (within_average - between_average[label[i]]) *  (within_average - between_average[label[i]]) * len(df[label[i]])
+    for i in range(len(label)):
+        within_heihouwa += between_variance[label[i]] * len(df[label[i]])
+    # print between_heihouwa
     # print within_heihouwa
 
-    between_heihouwa = between_variance * len(df[label].values.flatten())
-    # print between_heihouwa
-
-    # TODO calculate dof
+    ### calculate dof ###
     between_dof = len(label) - 1
-    #print between_dof
     within_dof = 0
     for i in range(len(label)):
-        #print len(label[i])
         within_dof += len(df[label[i]]) - 1
+    # print between_dof
     # print within_dof
 
-    # TODO calculate heikin heihou
+    ### calculate heikin heihou ###
     between_heikin_heihou = between_heihouwa / between_dof
     within_heikin_heihou = within_heihouwa / within_dof
-    print between_heikin_heihou
-    print within_heikin_heihou
+    # print between_heikin_heihou
+    # print within_heikin_heihou
 
-    # TODO calculate F value
+    ### calculate F value ###
     F = between_heikin_heihou / within_heikin_heihou
-    print F
+    # print F
+
+    print "one-way anova F value: " + str(F) + "\n degree of freedom between group: " + str(between_dof) + "\n degree of freedom within group: " + str(within_dof)     
+
 
 if __name__ == '__main__':
-    data = {'Japanese':  [68, 75, 80, 71, 73, 79, 69, 65],
-            'Mathematics': [86, 83, 76, 81, 75, 82, 87, 75],
-            'Science' : [85, 69, 77, 77, 75, 74, 87, 69],
-            'English': [80, 76, 84, 93, 76, 80, 79, 84]}
-    df = DataFrame(data, index = ["Student" + str(i+1)  for i  in np.arange(8)])
+    # data = {'Japanese':  [68, 75, 80, 71, 73, 79, 69, 65],
+    #         'Mathematics': [86, 83, 76, 81, 75, 82, 87, 75],
+    #         'Science' : [85, 69, 77, 77, 75, 74, 87, 69],
+    #         'English': [80, 76, 84, 93, 76, 80, 79, 84]}
+    data = {'Japanese':  [80, 75, 80, 90, 95, 80, 80, 85, 85, 80, 90, 80, 75, 90, 85, 85, 90, 90, 85, 80],
+            'Mathematics': [75, 70, 80, 85, 90, 75, 85, 80, 80, 75, 80, 75, 70, 85, 80, 75, 80, 80, 90, 80],
+            'Science' : [80, 80, 80, 90, 95, 85, 95, 90, 85, 90, 95, 85, 98, 95, 85, 85, 90, 90, 85, 85]}
+
+    df = DataFrame(data, index = [str(i+1)  for i  in np.arange(20)]) # must change the length of array
+
     main(df, ['Japanese', 'Mathematics', 'Science'])
