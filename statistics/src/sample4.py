5e0c5e84c0a5d3e9e2b89760af223c055fca1c97
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2017-04-17 14:55:14 +0900

    update two-way anova

--- statistics/src/sample4.py
+++ statistics/src/sample4.py
@@ -44,10 +44,10 @@ def main(data, label, condition):
     total_sum = sum1 + sum2 + sum3 + sum4
     total_len = len1 + len2 + len3 + len4
     
-    between_average.append(sum1 / len1)
-    between_average.append(sum2 / len2)
-    between_average.append(sum3 / len3)
-    between_average.append(sum4 / len4)
+    between_average.append(sum1 / len1) # crispy
+    between_average.append(sum2 / len2) # normal
+    between_average.append(sum3 / len3) # hot 
+    between_average.append(sum4 / len4) # mild
     between_average.append(total_sum / total_len)
     # print between_average
 
@@ -90,18 +90,54 @@ def main(data, label, condition):
         between_sum_of_squares.append(between_variance[i] * len(df[label[i]]))
  
     between_sum_of_squares.append(between_variance[condition * len(label)] * len(df[label]) * len(label))
-    print between_sum_of_squares
+    # print between_sum_of_squares
         
 
-    ### calculate sample num ###
+    ### calculate difference of factor1 ###
+    difference_of_factor1 = 0.0
+    difference_of_factor1 = math.pow((between_average[4] - between_average[8]), 2) * len1 + math.pow((between_average[5] - between_average[8]), 2) * len2
+    # print difference_of_factor1
 
-    # table = Texttable()
-    # table.set_cols_align(["c", "c", "c", "c", "c"])
-    # table.set_cols_valign(["m", "m", "m", "m", "m"])
-    # table.add_rows([ ["Factor", "Sum of Squares", "Dof", "Mean Square", "F"], 
-    #                  ["Between Groups", str(between_sum_of_squares), str(between_dof), str(between_mean_square), str(F)],
-    #                  ["Within Groups", str(within_sum_of_squares), str(within_dof), str(within_mean_square), ""] ])
-    # print table.draw()
+    ### calculate difference of factor2 ###
+    difference_of_factor2 = 0.0
+    difference_of_factor2 = math.pow((between_average[6] - between_average[8]), 2) * len3 + math.pow((between_average[7] - between_average[8]), 2) * len4
+    # print difference_of_factor2
+
+    ### calculate difference of interaction ###
+    difference_of_each_group = 0.0
+    for i in range(len(label)):
+        difference_of_each_group += math.pow((between_average[i] - between_average[8]), 2) * len(df[label[i]]) 
+
+    # print difference_of_each_group
+    
+    difference_of_interaction = 0.0
+    difference_of_interaction = difference_of_each_group - difference_of_factor1 - difference_of_factor2
+    print difference_of_interaction
+
+    ### calculate difference_of_others
+    difference_of_others = 0.0
+    for i in range(4):
+        difference_of_others += between_sum_of_squares[i]
+    print difference_of_others
+    
+    ### show a table ###
+    total_num = 0
+    for i in range(len(label)):
+        print i
+        total_num += len(df[label[i]])
+    print total_num
+
+    table = Texttable()
+    table.set_cols_align(["c", "c", "c", "c", "c"])
+    table.set_cols_valign(["m", "m", "m", "m", "m"])
+    table.add_rows([["Factor", "Sum of Squares", "Dof", "Mean Square", "F"], 
+                    ["Factor1", str(difference_of_factor1), str(1), str(difference_of_factor1 / 1.0), str((difference_of_factor1 / 1.0) / ((difference_of_others / (total_num -1 -1 -1 -1))))],
+                    ["Factor2", str(difference_of_factor2), str(1), str(difference_of_factor2 / 1.0), str((difference_of_factor2 / 1.0) / (difference_of_others / (total_num -1 -1 -1 -1)))],
+                    ["Interaction", str(difference_of_interaction), str(1), str(difference_of_interaction / 1.0), str((difference_of_interaction / 1.0) / (difference_of_others / (total_num -1 -1 -1 -1)))],
+                    ["Others", str(difference_of_others), str(total_num -1 -1 -1 -1), str(difference_of_others / (total_num -1 -1 -1 -1)), ""],
+                    ["Total", str(between_sum_of_squares[4]), str(total_num -1), "", ""]])
+    
+    print table.draw()
 
     # print "one-way anova F value: " + str(F) + "\n degree of freedom between group: " + str(between_dof) + "\n degree of freedom within group: " + str(within_dof)     
 
@@ -111,7 +147,7 @@ if __name__ == '__main__':
             'Crispy-mild': [65, 70, 80, 75, 70, 60, 65, 70, 85, 60, 65, 75, 70, 80, 75],
             'Normal-hot' : [70, 65, 85, 80, 75, 65, 75, 60, 85, 65, 75, 70, 65, 80, 75],
             'Normal-mild' : [70, 70, 85, 80, 65, 75, 65, 85, 80, 60, 70, 75, 70, 80, 85]
-}
+    }
 
     df = DataFrame(data, index = [str(i+1)  for i  in np.arange(15)]) # must change the length of array
 
