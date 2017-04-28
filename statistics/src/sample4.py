83c79750ff4af47c880fb35c784ad2331dade3f7
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2017-04-28 22:16:10 +0900

    expand 2*3

--- statistics/src/sample4.py
+++ statistics/src/sample4.py
@@ -151,31 +151,29 @@ def main(data, label, factor1, factor2, condition_number_of_factor1, condition_n
     table.set_cols_align(["c", "c", "c", "c", "c"])
     table.set_cols_valign(["m", "m", "m", "m", "m"])
     table.add_rows([["Factor", "Sum of Squares", "Dof", "Mean Square", "F"], 
-                    ["Factor1 (" + str(factor1_name) + ")", str(difference_of_factor1), str(condition_number_of_factor1 - 1), str(difference_of_factor1 / (condition_number_of_factor1 - 1.0)), str((difference_of_factor1 / (condition_number_of_factor1 - 1.0)) / ((difference_of_others / (total_num - (condition_number_of_factor1 - 1) - (len(label) - condition_number_of_factor1 -1) -1))))],
-                    ["Factor2 ("+ str(factor2_name) + ")", str(difference_of_factor2), str((len(label) / condition_number_of_factor1) - 1), str(difference_of_factor2 / (len(label) / condition_number_of_factor1) - 1), str((difference_of_factor2 / 1.0) / (difference_of_others / (total_num - (condition_number_of_factor1 - 1) - (len(label) - condition_number_of_factor1 -1) -1)))],
-                    #["Interaction", str(difference_of_interaction), str((condition_number_of_factor1 - 1.0) * ((len(label) / condition_number_of_factor1) - 1)), str(difference_of_interaction / (condition_number_of_factor1 - 1.0) * ((len(label) / condition_number_of_factor1) - 1)), str(difference_of_interaction / ((condition_number_of_factor1 - 1.0) * ((len(label) / condition_number_of_factor1) - 1)) / (difference_of_others / (total_num - (condition_number_of_factor1 - 1) - (len(label) - condition_number_of_factor1 -1) -1)))
-
-(difference_of_others / (total_num - (condition_number_of_factor1 - 1) - (len(label) - condition_number_of_factor1 -1) -1)))],
-                    ["Others", str(difference_of_others), str(total_num -1 -1 -1 -1), str(difference_of_others / (total_num - (condition_number_of_factor1 - 1) - (len(label) - condition_number_of_factor1 -1) -1)), ""],
+                    ["Factor1 (" + str(factor1_name) + ")", str(difference_of_factor1), str(condition_number_of_factor1 - 1), str(difference_of_factor1 / (condition_number_of_factor1 - 1.0)), str((difference_of_factor1 / (condition_number_of_factor1 - 1.0)) / ((difference_of_others / (total_num - (condition_number_of_factor1 - 1) - (len(label) / condition_number_of_factor1 -1) -1))))],
+                    ["Factor2 ("+ str(factor2_name) + ")", str(difference_of_factor2), str((len(label) / condition_number_of_factor1) - 1), str(difference_of_factor2 / (len(label) / condition_number_of_factor1) - 1), str((difference_of_factor2 / 1.0) / (difference_of_others / (total_num - (condition_number_of_factor1 - 1) - (len(label) / condition_number_of_factor1 -1) -1)))],
+                    ["Interaction", str(difference_of_interaction), str((condition_number_of_factor1 - 1.0) * ((len(label) / condition_number_of_factor1) - 1)), str(difference_of_interaction / ((condition_number_of_factor1 - 1.0) * ((len(label) / condition_number_of_factor1) - 1))), str((difference_of_interaction / ((condition_number_of_factor1 - 1.0) * ((len(label) / condition_number_of_factor1) - 1))) / (difference_of_others / (total_num - (condition_number_of_factor1 - 1) - (len(label) / condition_number_of_factor1 -1) -1)))],
+                    ["Others", str(difference_of_others), str(total_num - (condition_number_of_factor1 - 1) - ((len(label) / condition_number_of_factor1) - 1) - ((condition_number_of_factor1 - 1.0) * ((len(label) / condition_number_of_factor1) - 1))  -1), str(difference_of_others / (total_num - (condition_number_of_factor1 - 1) - (len(label) / condition_number_of_factor1 -1) -1)), ""],
                     ["Total", str(between_sum_of_squares[len(between_sum_of_squares) - 1]), str(total_num -1), "", ""]])
     
     print table.draw()
     
 
 if __name__ == '__main__':
-    # data = {'Crispy-hot':  [65, 85, 75, 85, 75, 80, 90, 75, 85, 65, 75, 85, 80, 85, 90],
-    #         'Crispy-mild': [65, 70, 80, 75, 70, 60, 65, 70, 85, 60, 65, 75, 70, 80, 75],
-    #         'Normal-hot' : [70, 65, 85, 80, 75, 65, 75, 60, 85, 65, 75, 70, 65, 80, 75],
-    #         'Normal-mild' : [70, 70, 85, 80, 65, 75, 65, 85, 80, 60, 70, 75, 70, 80, 85]
-    #     }
-
     data = {'Crispy-hot':  [65, 85, 75, 85, 75, 80, 90, 75, 85, 65, 75, 85, 80, 85, 90],
-            'Crispy-normal': [65, 70, 80, 75, 70, 60, 65, 70, 85, 60, 65, 75, 70, 80, 75],
             'Crispy-mild': [65, 70, 80, 75, 70, 60, 65, 70, 85, 60, 65, 75, 70, 80, 75],
             'Normal-hot' : [70, 65, 85, 80, 75, 65, 75, 60, 85, 65, 75, 70, 65, 80, 75],
-            'Normal-normal' : [70, 65, 85, 80, 75, 65, 75, 60, 85, 65, 75, 70, 65, 80, 75],
             'Normal-mild' : [70, 70, 85, 80, 65, 75, 65, 85, 80, 60, 70, 75, 70, 80, 85]
         }
 
-    main(data, ['Crispy-hot', 'Crispy-normal', 'Crispy-mild', 'Normal-hot', 'Normal-normal', 'Normal-mild'], "Texture", "Flavor", 2, 3)
-    # main(data, ['Crispy-hot', 'Crispy-mild', 'Normal-hot', 'Normal-mild'], "Texture", "Flavor", 2, 2)
+    # data = {'Crispy-hot':  [65, 85, 75, 85, 75, 80, 90, 75, 85, 65, 75, 85, 80, 85, 90],
+    #         'Crispy-normal': [65, 70, 80, 75, 70, 60, 65, 70, 85, 60, 65, 75, 70, 80, 75],
+    #         'Crispy-mild': [65, 70, 80, 75, 70, 60, 65, 70, 85, 60, 65, 75, 70, 80, 75],
+    #         'Normal-hot' : [70, 65, 85, 80, 75, 65, 75, 60, 85, 65, 75, 70, 65, 80, 75],
+    #         'Normal-normal' : [70, 65, 85, 80, 75, 65, 75, 60, 85, 65, 75, 70, 65, 80, 75],
+    #         'Normal-mild' : [70, 70, 85, 80, 65, 75, 65, 85, 80, 60, 70, 75, 70, 80, 85]
+    #     }
+
+    #main(data, ['Crispy-hot', 'Crispy-normal', 'Crispy-mild', 'Normal-hot', 'Normal-normal', 'Normal-mild'], "Texture", "Flavor", 2, 3)
+    main(data, ['Crispy-hot', 'Crispy-mild', 'Normal-hot', 'Normal-mild'], "Texture", "Flavor", 2, 2)
