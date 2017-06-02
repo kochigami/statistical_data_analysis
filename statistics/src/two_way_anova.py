e2a24976121323455af99812a46c1b6c1ec34156
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2017-06-02 10:41:18 +0900

    add more comments

--- statistics/src/two_way_anova.py
+++ statistics/src/two_way_anova.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from pandas import DataFrame
-#from scipy import stats
-#import matplotlib.pyplot as plt
 import numpy as np
 import math
-#from texttable import Texttable
 from calculate_average import CalculateAverage
 from calculate_variance import CalculateVariance
 from analysis_of_variance import AnalysisOfVariance
@@ -24,7 +21,7 @@ class TwoWayAnova:
            df_of_each_group: DataFrame of each category
            label_of_each_group: string list. label of each category
         Example:
-           within_average: [2.0296623333333335, 1.9834588333333334, 2.074698666666667]
+           within_average: [79.66666666666667, 71.0, 71.0, 72.66666666666667, 72.66666666666667, 74.33333333333333]
         '''
         within_average = []
         for i in range(len(label_of_each_group)):
@@ -35,7 +32,7 @@ class TwoWayAnova:
            df_factor1: DataFrame of factor1
            label_factor1: string list. label of factor1
         Example:
-           factor1_average: [2.0296623333333335, 1.98]
+           factor1_average: [73.88888888888889, 73.22222222222223]
         '''
         factor1_average = []
         for i in range(len(label_factor1)):
@@ -46,7 +43,7 @@ class TwoWayAnova:
            df_factor2: DataFrame of factor2
            label_factor2: string list. label of factor2
         Example:
-           factor2_average: [2.0296623333333335, 1.98]
+           factor2_average: [76.16666666666667, 71.83333333333333, 72.66666666666667]
         '''
         factor2_average = []
         for i in range(len(label_factor2)):
@@ -57,10 +54,10 @@ class TwoWayAnova:
            df_of_all_samples: DataFrame of all samples
            label_of_all_samples: string list. Normally, ["all"].
         Example:
-           average_of_all: [2.0296623333333335]
+           average_of_all: 73.5555555556
         '''
         average_of_all = self.calculate_average.calc_average(df_of_all_samples, label_of_all_samples)
-        
+
         ### calculate sample nums ###
         '''
         Calculate sample_num_per_group: float list. sample num of each category
@@ -68,7 +65,7 @@ class TwoWayAnova:
            df_of_each_group: DataFrame of each category
            label_of_each_group: string list. label of each category
         Example:
-           sample_num_per_group: [6.0, 6.0, 6.0]
+           sample_num_per_group: [15.0, 15.0, 15.0, 15.0, 15.0, 15.0]
         '''
         sample_num_per_group =[]
         sample_num_per_group = self.analysis_of_variance.calc_sample_num(df_of_each_group, label_of_each_group, sample_num_per_group)
@@ -78,7 +75,7 @@ class TwoWayAnova:
            df_factor1: DataFrame of factor1
            label_factor1: string list. label of factor1
         Example:
-           sample_num_per_factor1: [6.0, 6.0, 6.0]
+           sample_num_per_factor1: [45.0, 45.0]
         '''
         sample_num_per_factor1 = []
         sample_num_per_factor1 = self.analysis_of_variance.calc_sample_num(df_factor1, label_factor1, sample_num_per_factor1)
@@ -88,7 +85,7 @@ class TwoWayAnova:
            df_factor2: DataFrame of factor2
            label_factor2: string list. label of factor2
         Example:
-           sample_num_per_factor2: [6.0, 6.0, 6.0]
+           sample_num_per_factor2: [30.0, 30.0, 30.0]
         '''
         sample_num_per_factor2 = []
         sample_num_per_factor2 = self.analysis_of_variance.calc_sample_num(df_factor2, label_factor2, sample_num_per_factor2)
@@ -98,7 +95,7 @@ class TwoWayAnova:
            df_of_all_samples: DataFrame of all samples
            label_of_all_samples: string list. Normally, ["all"].
         Example:
-           total_sample_num: [18.0]
+           total_sample_num: [90.0]
         '''
         total_sample_num = []
         total_sample_num = self.analysis_of_variance.calc_sample_num(df_of_all_samples, label_of_all_samples, total_sample_num)
@@ -110,7 +107,7 @@ class TwoWayAnova:
            df_of_each_group: DataFrame of each category
            label_of_each_group: string list. label of each category
         Example:
-           within_variance: [0.03470865617688887, 0.1543511475384722, 0.2957196379978889]
+           within_variance: [58.222222222222214, 50.666666666666664, 50.666666666666664, 56.222222222222214, 56.222222222222214, 59.55555555555555]
         '''
         within_variance = []
         for i in range(len(label_of_each_group)):
@@ -121,7 +118,7 @@ class TwoWayAnova:
            df_factor1: DataFrame of factor1
            label_of_factor1: string list. label of factor1
         Example:
-           factor1_variance: [0.03470865617688887, 0.1543511475384722, 0.2957196379978889]
+           factor1_variance: [69.87654320987652, 57.95061728395062]
         '''
         factor1_variance = []
         for i in range(len(label_factor1)):
@@ -132,7 +129,7 @@ class TwoWayAnova:
            df_factor2: DataFrame of factor2
            label_of_factor2: string list. label of factor2
         Example:
-           factor2_variance: [0.03470865617688887, 0.1543511475384722, 0.2957196379978889]
+           factor2_variance: [69.47222222222221, 54.138888888888886, 57.88888888888888]
         '''
         factor2_variance = []
         for i in range(len(label_factor2)):
@@ -143,10 +140,10 @@ class TwoWayAnova:
            df_of_all_samples: DataFrame of all samples
            label_of_all_samples: string list. Normally, ["all"].
         Example:
-           variance_of_all: 0.162980674118
+           variance_of_all: 64.024691358
         '''
         variance_of_all = self.calculate_variance.calc_variance(df_of_all_samples, label_of_all_samples)
-        
+
         ### calculate sum of squares ###
         '''
         Calculate sum_of_squares_of_others: total sum of squares (float) of others 
@@ -154,7 +151,7 @@ class TwoWayAnova:
            sample_num_per_group: calculated from "calculate sample nums" (float list)
            within_variance: calculated from "calculate variance" (float list) 
         Example:
-           sum_of_squares_of_others: 2.90867665028 
+           sum_of_squares_of_others: 4973.33333333 
         '''
         sum_of_squares_of_others = 0.0
         for i in range(len(label_of_each_group)):
@@ -166,7 +163,7 @@ class TwoWayAnova:
            average_of_all: float.
            within_variance: calculated from "calculate variance" (float list) 
         Example:
-           sum_of_squares_of_each_group: 2.90867665028 
+           sum_of_squares_of_each_group: 788.888888889
         '''
         sum_of_squares_of_each_group = 0.0
         for i in range(len(label_of_each_group)):
@@ -178,10 +175,10 @@ class TwoWayAnova:
            average_of_all: float.
            factor1_variance: calculated from "calculate variance" (float list) 
         Example:
-           sum_of_squares_of_factor1: 2.90867665028 
+           sum_of_squares_of_factor1: 10.0
         '''
         sum_of_squares_of_factor1 = 0.0
-        for i in range(len(label_factor1)):
+        for i in range(len(label_factor1)): 
             sum_of_squares_of_factor1 += math.pow((factor1_average[i] - average_of_all), 2) * sample_num_per_factor1[i]
         '''
         Calculate sum_of_squares_of_factor2: total sum of squares (float) of factor2
@@ -190,7 +187,7 @@ class TwoWayAnova:
            average_of_all: float.
            factor2_variance: calculated from "calculate variance" (float list) 
         Example:
-           sum_of_squares_of_factor2: 2.90867665028 
+           sum_of_squares_of_factor2: 317.222222222 
         '''
         sum_of_squares_of_factor2 = 0.0
         for i in range(len(label_factor2)):
@@ -202,7 +199,7 @@ class TwoWayAnova:
            sum_of_squares_of_factor1: float
            sum_of_squares_of_factor2: float
         Example:
-           sum_of_squares_of_interaction: 2.90867665028 
+           sum_of_squares_of_interaction: 461.666666667
         '''
         sum_of_squares_of_interaction = sum_of_squares_of_each_group - sum_of_squares_of_factor1 - sum_of_squares_of_factor2
         
@@ -212,7 +209,7 @@ class TwoWayAnova:
         Args:
            sample_num_per_factor1: calculated from "calculate sample nums"
         Example:
-           dof_of_factor1: 2.0
+           dof_of_factor1: 1.0
         '''
         dof_of_factor1 = len(sample_num_per_factor1) - 1.0
         '''
@@ -237,7 +234,7 @@ class TwoWayAnova:
         Args:           
            total_sample_num: args (float list)
         Example:
-           dof_of_all: 2.0
+           dof_of_all: 89.0
         '''
         dof_of_all = total_sample_num[0] - 1.0
         '''
@@ -248,7 +245,7 @@ class TwoWayAnova:
            dof_of_factor2:
            dof_of_interaction:
         Example:
-           dof_of_others: 2.0
+           dof_of_others: 84.0
         '''
         dof_of_others = dof_of_all - dof_of_factor1 - dof_of_factor2 - dof_of_interaction
 
@@ -259,7 +256,7 @@ class TwoWayAnova:
            sum_of_squares_of_factor1: calculated from "calculate sum of squares" (float)
            dof_of_factor1: calculated from "calculate dof" (float)
         Example:
-           mean_square_of_factor1: 0.012
+           mean_square_of_factor1: 10.0
         '''
         mean_square_of_factor1 = 0.0
         mean_square_of_factor1 = self.analysis_of_variance.calc_mean_square(sum_of_squares_of_factor1, dof_of_factor1)
@@ -269,7 +266,7 @@ class TwoWayAnova:
            sum_of_squares_of_factor2: calculated from "calculate sum of squares" (float)
            dof_of_factor1: calculated from "calculate dof" (float)
         Example:
-           mean_square_of_factor2: 0.012
+           mean_square_of_factor2: 158.611111111
         '''
         mean_square_of_factor2 = 0.0        
         mean_square_of_factor2 = self.analysis_of_variance.calc_mean_square(sum_of_squares_of_factor2, dof_of_factor2)
@@ -279,7 +276,7 @@ class TwoWayAnova:
            sum_of_squares_of_interaction: calculated from "calculate sum of squares" (float)
            dof_of_interaction: calculated from "calculate dof" (float)
         Example:
-           mean_square_of_interaction: 0.012
+           mean_square_of_interaction: 230.833333333
         '''        
         mean_square_of_interaction = 0.0
         mean_square_of_interaction = self.analysis_of_variance.calc_mean_square(sum_of_squares_of_interaction, dof_of_interaction)
@@ -289,7 +286,7 @@ class TwoWayAnova:
            sum_of_squares_of_others: calculated from "calculate sum of squares" (float)
            dof_of_others: calculated from "calculate dof" (float)
         Example:
-           mean_square_of_others: 0.012
+           mean_square_of_others: 59.2063492063
         '''
         mean_square_of_others = 0.0
         mean_square_of_others = self.analysis_of_variance.calc_mean_square(sum_of_squares_of_others, dof_of_others)
@@ -301,7 +298,7 @@ class TwoWayAnova:
            mean_square_of_factor1: calculated from "calculate mean square" (float)
            mean_square_of_others: calculated from "calculate mean square" (float)
         Example: 
-           F_of_factor1: 0.064
+           F_of_factor1: 0.16890080429
         '''
         F_of_factor1 = self.analysis_of_variance.calc_F(mean_square_of_factor1, mean_square_of_others)
         '''
@@ -310,7 +307,7 @@ class TwoWayAnova:
            mean_square_of_factor2: calculated from "calculate mean square" (float)
            mean_square_of_others: calculated from "calculate mean square" (float)
         Example: 
-           F_of_factor2: 0.064
+           F_of_factor2: 2.67895442359
         '''
         F_of_factor2 = self.analysis_of_variance.calc_F(mean_square_of_factor2, mean_square_of_others)
         '''
@@ -319,7 +316,7 @@ class TwoWayAnova:
            mean_square_of_interaction: calculated from "calculate mean square" (float)
            mean_square_of_others: calculated from "calculate mean square" (float)
         Example: 
-           F_of_interaction: 0.064
+           F_of_interaction: 3.89879356568
         '''
         F_of_interaction = self.analysis_of_variance.calc_F(mean_square_of_interaction, mean_square_of_others)
 
