785567d42cae0f766a5cc41a2ec572ca28cdb1a7
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2017-06-07 11:26:55 +0900

    add anova class for one-/two-way anova

---
+++ statistics/src/anova.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from analysis_of_variance import AnalysisOfVariance
+from one_way_anova import OneWayAnova
+from two_way_anova import TwoWayAnova
+
+class ANOVA:
+    def one_way_anova(self, data):
+        """
+        Args:
+        data = {'Japanese':  [68, 75, 80, 71, 73, 79, 69, 65],
+        'Mathematics': [86, 83, 76, 81, 75, 82, 87, 75],
+        'Science' : [85, 69, 77, 77, 75, 74, 87, 69],
+        'English': [80, 76, 84, 93, 76, 80, 79, 84]}
+        """
+        analysis_of_variance = AnalysisOfVariance()
+        df = analysis_of_variance.create_df(data)
+        df_label = analysis_of_variance.create_label(df)
+        df_all = analysis_of_variance.create_df(data, "all")
+        df_all_label = analysis_of_variance.create_label(df_all)
+        one_way_anova = OneWayAnova()
+        one_way_anova.calc_one_way_anova(df, df_label, df_all, df_all_label)
+
+    def two_way_anova(self, data, factor1_label, factor2_label):
+        """
+        Args:
+        data = {'Crispy-hot':  [65, 85, 75, 85, 75, 80, 90, 75, 85, 65, 75, 85, 80, 85, 90],
+        'Crispy-mild': [65, 70, 80, 75, 70, 60, 65, 70, 85, 60, 65, 75, 70, 80, 75],
+        'Normal-hot' : [70, 65, 85, 80, 75, 65, 75, 60, 85, 65, 75, 70, 65, 80, 75],
+        'Normal-mild' : [70, 70, 85, 80, 65, 75, 65, 85, 80, 60, 70, 75, 70, 80, 85]}
+        factor1_label: ["Crispy", "Normal"]
+        factor2_label: ["hot", "mild"]
+        
+        %Be careful for names of each factor.
+        Each name shouldn't include other.
+        For example, "vision", "sound", "vision+sound" is bad because thrid one (vision+sound) includes first (vision) and second one (sound).
+        In this case, "vision", "sound", "both" is good.  
+        
+        """
+
+        analysis_of_variance = AnalysisOfVariance()
+        
+        df_factor1 = analysis_of_variance.create_df(data, factor1_label)
+        df_factor2 = analysis_of_variance.create_df(data, factor2_label)
+        
+        df = analysis_of_variance.create_df(data)
+        df_label = analysis_of_variance.create_label(df)
+        df_factor1_label = analysis_of_variance.create_label(df_factor1)
+        df_factor2_label = analysis_of_variance.create_label(df_factor2)
+        df_all = analysis_of_variance.create_df(data, "all")
+        df_all_label = analysis_of_variance.create_label(df_all)
+        two_way_anova = TwoWayAnova()
+        two_way_anova.calc_two_way_anova(df, df_label, df_factor1, df_factor1_label, df_factor2, df_factor2_label, df_all, df_all_label)
