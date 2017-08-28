7f91e4e59f47be3eeba169882330ec5bed0f5928
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2017-08-28 11:27:45 +0900

    add welch's test

--- statistics/src/draw/draw_graph.py
+++ statistics/src/draw/draw_graph.py
@@ -93,7 +93,7 @@ class DrawGraph:
         elif mode == "paired-ttest":
             new_title = title + "\n(N = " + str(len(data[(data.keys())[0]])) + " for each type, * p < 0.1, ** p < 0.05)"
         elif mode == "unpaired-ttest":
-            new_title = title + "\n(N = " + str(len(data[(data.keys())[0]])) * len(data.keys()) + " for total, * p < 0.1, ** p < 0.05)"
+            new_title = title + "\n(N = " + str(len(data[(data.keys())[0]]) + len(data[(data.keys())[1]]))  + " for total, * p < 0.1, ** p < 0.05)"
         plt.title(new_title)
         plt.xlabel(xlabel)
         plt.ylabel(ylabel)
@@ -144,6 +144,12 @@ if __name__ == '__main__':
     else:
         d = DrawGraph()
         if args[1] == "unpaired-ttest":
+            # for welch's test
+            # followed this website for sample: http://www.geisya.or.jp/~mwm48961/statistics/ttest_question2.htm
+            # data = {'HamburgerA':  [15.3, 14.9, 14.5, 14.4, 14.0, 13.9, 14.1, 14.7, 15.3, 14.6],
+            #         'HamburgerB' : [13.9, 14.2, 14.1, 14.3, 14.1, 13.7, 14.7, 13.9, 14.1, 13.8, 14.3]}
+            
+            # for student's test
             # followed this website for sample: http://kogolab.chillout.jp/elearn/hamburger/chap4/sec1.html
             data = {'HamburgerA':  [70, 75, 70, 85, 90, 70, 80, 75],
                     'HamburgerB' : [85, 80, 95, 70, 80, 75, 80, 90]}
--- statistics/src/t_test/t_test.py
+++ statistics/src/t_test/t_test.py
@@ -2,6 +2,12 @@
 # -*- coding: utf-8 -*-
 from scipy import stats
 import numpy as np
+import math
+from scipy.stats import t as calc_p
+# referenced as calc_p because of the error below:
+# File "/home/kochigami/my_tutorial/statistics/src/t_test/t_test.py", line 80, in unpaired_ttest
+# p = t.sf(t_value, dof)
+# UnboundLocalError: local variable 't' referenced before assignment
 
 class TTEST:
     def paired_ttest(self, data):
@@ -13,17 +19,22 @@ class TTEST:
         """
         If samples are paired,
         we use paired t-test
+        I followed this website for calculation: 
+        student's test: http://kogolab.chillout.jp/elearn/hamburger/chap5/sec3.html
         """
         # calculate t & p value
-        t, p = stats.ttest_rel(data[(data.keys())[0]], data[(data.keys())[1]])
-        print( "p value = %(p)s" %locals() )
-        return p
+        if len(data[(data.keys())[0]]) == len(data[(data.keys())[1]]):
+            t, p = stats.ttest_rel(data[(data.keys())[0]], data[(data.keys())[1]])
+            print ("p value = %(p)s" %locals() )
+            return p
+        else:
+            print "Please make sure the sample num is same in two conditions."
+            return False
 
     def unpaired_ttest(self, data):
         """
         data = {'Japanese':  [68, 75, 80, 71, 73, 79, 69, 65],
                 'English': [80, 76, 84, 93, 76, 80, 79, 84]}
-        label: ["Japanese", "English"] 
         """
         """
         If samples are not paired,
@@ -31,15 +42,26 @@ class TTEST:
         We have to investigate whether the distribution is equall (tou bunsan) 
         by calculating f.
         We have to choose larger one as numerator (bunshi)
+        I followed this website for calculation: 
+        student's test: http://kogolab.chillout.jp/elearn/hamburger/chap4/sec3.html
+        welch's test: http://hs-www.hyogo-dai.ac.jp/%7Ekawano/HStat/?2015%2F13th%2FWelch%27s_Test
         """
-        if np.var(data[(data.keys())[0]]) > np.var(data[(data.keys())[1]]):
-            f = np.var(data[(data.keys())[0]]) / np.var(data[(data.keys())[1]])
-        else:
-            f = np.var(data[(data.keys())[1]]) / np.var(data[(data.keys())[0]])
         dfx = len(data[(data.keys())[0]]) - 1
         dfy = len(data[(data.keys())[1]]) - 1
-        p_value = stats.f.cdf(f, dfx, dfy)
-
+        s_x = (1.0 / (len(data[(data.keys())[0]]) - 1.0)) * np.var(data[(data.keys())[0]])
+        s_y = (1.0 / (len(data[(data.keys())[1]]) - 1.0)) * np.var(data[(data.keys())[1]])
+        if s_x > s_y:
+            # Usually, f value should be greater than 1.0,
+            # I reverse s_x and s_y upside down
+            # in order to let stats.f.cdf return p value less than 0.05 for example
+            # % If I use s_x / s_y instead of s_y / s_x, it returns 0.96 instead of 0.03
+            # % but I need the value less than 0.05.
+            f = s_y / s_x
+            p_value = stats.f.cdf(f, dfx, dfy)
+        else:
+            f = s_x / s_y
+            p_value = stats.f.cdf(f, dfy, dfx)
+            
         """
         After obtaining p value, 
         we can check whether the distribution of samples is equal or not.
@@ -48,11 +70,33 @@ class TTEST:
         """
         # calculate t & p value
         if p_value < 0.05:
-            t, p = stats.ttest_ind(data[(data.keys())[0]], data[(data.keys())[1]], equal_var = False)
+            # welch's test
+            diff_average = np.average(data[(data.keys())[0]]) - np.average(data[(data.keys())[1]])
+            sample_variance_1 = 0.0
+            for i in range(len(data[(data.keys())[0]])):
+                sample_variance_1 += pow(((data[(data.keys())[0]])[i] - np.average(data[(data.keys())[0]])), 2.0)
+            sample_variance_1 = (1.0 / (len(data[(data.keys())[0]]) - 1)) * sample_variance_1
+
+            sample_variance_2 = 0.0
+            for i in range(len(data[(data.keys())[1]])):
+                sample_variance_2 += pow(((data[(data.keys())[1]])[i] - np.average(data[(data.keys())[1]])), 2.0)
+            sample_variance_2 = (1.0 / (len(data[(data.keys())[1]]) - 1)) * sample_variance_2
+
+            t = diff_average / math.sqrt((sample_variance_1 / len(data[(data.keys())[0]])) + (sample_variance_2 / len(data[(data.keys())[1]])))
+            
+            dof = pow((sample_variance_1 / len(data[(data.keys())[0]])) + (sample_variance_2 / len(data[(data.keys())[1]])) , 2.0) / ((pow(sample_variance_1 / len(data[(data.keys())[0]]), 2.0) / (len(data[(data.keys())[0]]) - 1.0)) + ((pow(sample_variance_2 / len(data[(data.keys())[1]]), 2.0) / (len(data[(data.keys())[1]]) - 1.0))))
+            dof = math.ceil(dof)
+            p = calc_p.sf(t, dof)
             ### heteroscedasticity: hi tou bunsan
             print ("t-test with heteroscedasticity")
         else:
-            t, p = stats.ttest_ind(data[(data.keys())[0]], data[(data.keys())[1]], equal_var = True)
+            # student's test
+            diff_average = np.average(data[(data.keys())[0]]) - np.average(data[(data.keys())[1]])
+            diff_variance = (np.var(data[(data.keys())[0]]) * len(data[(data.keys())[0]]) + np.var(data[(data.keys())[1]]) * len(data[(data.keys())[1]])) / (len(data[(data.keys())[0]]) + len(data[(data.keys())[1]]) -2)
+            diff_error = math.sqrt (diff_variance * ((1.0 / len(data[(data.keys())[0]]) + (1.0 / len(data[(data.keys())[1]])))))
+            t = diff_average / diff_error
+            dof = len(data[(data.keys())[0]]) + len(data[(data.keys())[1]]) -2
+            p = calc_p.sf(t, dof)
             ### homoscedasticity: tou bunsan
             print ("t-test with homoscedasticity")
 
