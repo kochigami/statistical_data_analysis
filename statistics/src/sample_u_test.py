be6832669107e1cd60d4694ccd85195025ae33d0
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2017-11-06 13:08:18 +0900

    add sign test

--- statistics/src/sample_u_test.py
+++ statistics/src/sample_u_test.py
@@ -6,18 +6,27 @@ from collections import OrderedDict
 
 if __name__ == '__main__':
     args = sys.argv
-    if len(args) is not 1:
-        print "python sample_u_test.py"
+    if len(args) is not 2:
+        print "python sample_u_test.py <sample_type>"
+        print "please choose sample type: "
+        print "1: paired-utest (sign test)"
+        print "2: unpaired-ttest (mann-whitney test)"
     else:
         d = DrawGraph()
         # if we use normal dict, the order of contents sometimes is decided randomly.
         # ex: should be [A, B, C], but output is [A, C, B]
         data = OrderedDict()
-        data['Children'] = [20, 18, 15, 13, 10, 6]
-        data['Adults'] = [17, 16, 12, 9, 8, 6, 4, 2]
-        d.draw_graph(data, "test", "x", "y", tight_layout=True, test_mode="utest")
-        
-        # if we use OrderedDict, but if we use the initialization below, 
-        # the order of contents sometimes is still decided randomly.
-        # data = {'HamburgerA':  [15.3, 14.9, 14.5, 14.4, 14.0, 13.9, 14.1, 14.7, 15.3, 14.6],
-        #         'HamburgerB' : [13.9, 14.2, 14.1, 14.3, 14.1, 13.7, 14.7, 13.9, 14.1, 13.8, 14.3]}
+        if args[1] == "1":
+            data['Cusine_A'] = [5, 3, 4, 4, 3, 4, 4, 1, 3, 3, 5, 3]
+            data['Cusine_B'] = [3, 5, 3, 3, 5, 2, 2, 1, 4, 2, 2, 3]
+            d.draw_graph(data, "test", "x", "y", tight_layout=True, test_mode="paired-utest")
+
+        elif args[1] == "2":
+            data['Children'] = [20, 18, 15, 13, 10, 6]
+            data['Adults'] = [17, 16, 12, 9, 8, 6, 4, 2]
+            d.draw_graph(data, "test", "x", "y", tight_layout=True, test_mode="unpaired-utest")
+
+            # if we use OrderedDict, but if we use the initialization below,
+            # the order of contents sometimes is still decided randomly.
+            # data = {'HamburgerA':  [15.3, 14.9, 14.5, 14.4, 14.0, 13.9, 14.1, 14.7, 15.3, 14.6],
+            #         'HamburgerB' : [13.9, 14.2, 14.1, 14.3, 14.1, 13.7, 14.7, 13.9, 14.1, 13.8, 14.3]}
--- statistics/src/u_test.py
+++ statistics/src/u_test.py
@@ -2,9 +2,69 @@
 # -*- coding: utf-8 -*-
 from scipy import stats
 import sys
+import itertools
 
 class UTEST:
-    def mann_whitney_utest(self, data):
+    def paired_utest(self, data):
+        """
+        data = {'Cusine_A': [5, 3, 4, 4, 3, 4, 4, 1, 3, 3, 5, 3]
+                'Cusine_B': [3, 5, 3, 3, 5, 2, 2, 1, 4, 2, 2, 3]}
+        # https://kusuri-jouhou.com/statistics/fugou.html
+       """
+        if len(data[(data.keys())[0]]) != len(data[(data.keys())[1]]):
+            print "Please check the contents of your data."
+            print "The number of data type should be two."
+            sys.exit()
+        else:
+            diff = []
+            for i in range(len(data[(data.keys())[0]])):
+                diff.append(data[(data.keys())[0]][i] - data[(data.keys())[1]][i])         
+            count_plus = 0
+            count_minus = 0
+            for i in range(len(diff)):
+                if diff[i] > 0.0:
+                    count_plus += 1
+                if diff[i] < 0.0:
+                    count_minus += 1
+            # choose smallest one among count_plus and count_minus
+            if count_plus < count_minus or count_plus == count_minus:
+                r = count_plus
+            else:
+                r = count_minus
+            # emit the pair if diff == 0
+            n = count_plus + count_minus
+
+            # if 5 < n < 25 or n == 25, execute below
+            # else (n > 25): calculate z (TODO)
+            # Please follow this link:
+            # https://kusuri-jouhou.com/statistics/fugou.html
+            p = 0.0
+            # hypothesis: there is no difference between condition A & B
+            #            = the probability that smallest one (r) appeared is 1/2
+            # so caluculate the probability that the value appears which is smaller than r 
+            # for loop from 0 to r
+            for i in range(0, r+1):
+                # [0] * 5 = [0, 0, 0, 0, 0]
+                # one of the initializing list
+                # list(itertools.combinations(['a', 'b', 'c', 'd', 'e'], 3))
+                # >> [('a', 'b', 'c'),
+                #     ('a', 'b', 'd'),
+                #     ('a', 'b', 'e'),
+                #     ('a', 'c', 'd'),
+                #     ('a', 'c', 'e'),
+                #     ('a', 'd', 'e'),
+                #     ('b', 'c', 'd'),
+                #     ('b', 'c', 'e'),
+                #     ('b', 'd', 'e'),
+                #     ('c', 'd', 'e')]
+                # len(list(itertools.combinations(['a', 'b', 'c', 'd', 'e'], 3)))
+                # >> 10
+                p += len(list(itertools.combinations([0] * n, i))) * pow(0.5, n)
+            # for two-side test, p value should be doubled
+            p = p * 2.0
+            return p
+
+    def unpaired_utest(self, data):
         """
         data = {'Children':  [20, 18, 15, 13, 10, 6],
                 'Adults': [17, 16, 12, 9, 8, 6, 4, 2]}
