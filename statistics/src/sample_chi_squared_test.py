ade9aac19a889d353e275d701ebc37b7dfd9c58c
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2017-11-06 18:36:31 +0900

    add sample

--- statistics/src/sample_chi_squared_test.py
+++ statistics/src/sample_chi_squared_test.py
@@ -16,10 +16,27 @@ if __name__ == '__main__':
         d = ChiSquaredTestDrawGraph()
         # Q2
         # data = np.array([[26, 27],[7, 10]])
-        # Q3 children
+        # Q2 children
         data = np.array([[43, 28],[19, 52]])
+        # Q2 adults
+        # data = np.array([[8, 12],[11, 9]])
+        # Q3 children
+        # data = np.array([[4, 7],[4, 10]])
         # Q3 adults
-        #data = np.array([[8, 12],[11, 9]])
-        d.draw_graph(data, ["Condition1", "Condition2"], "test", "x", "y", tight_layout=True, mode="paired")
+        # data = np.array([[4, 6],[3, 7]])
+        # enquette
+        # data = np.array([[20, 9],[18, 9]])
+        # data = np.array([[21, 9],[17, 9]])
+
+        # data = np.array([[7, 22],[16, 11]])
+        # data = np.array([[15, 15],[8, 18]])
+
+        # data = np.array([[16, 13],[12, 15]])
+        # data = np.array([[16, 14],[12, 14]])
+
+        # data = np.array([[9, 20],[15, 12]])
+        data = np.array([[15, 12],[11, 15]])
+        
+        d.draw_graph(data, ["Condition1", "Condition2"], "test", "x", "y", tight_layout=True, mode="unpaired")
         # TODO
         # draw table        
