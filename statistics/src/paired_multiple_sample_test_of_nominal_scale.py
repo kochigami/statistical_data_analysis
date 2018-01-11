7d92c4cfec86bac5d89e85f720fc6f3c9728d99d
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2018-01-11 10:58:59 +0900

    add cochrans's Q test with multiple samples

---
+++ statistics/src/paired_multiple_sample_test_of_nominal_scale.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from scipy import stats
+import sys
+
+"""
+Cochran's Q test
+"""
+
+class PairedMultipleSampleTestOfNominalScale:
+    def test(self, data):
+        """
+        data = [[1,1,1], [1,1,1], [0,1,1], [0,1,1], [0,1,1], [0,0,1], [0,0,1], [0,0,1], [0,0,1], [0,0,0], [0,0,0]]
+        example
+                  CandidateA  CandidateB  CandidateC  Total (sum_row)
+        ---------------------------------------------------
+        Subject1      1           1           1        3
+        Subject2      1           1           1        3     
+        Subject3      0           1           1        2     
+        Subject4      0           1           1        2
+        Subject5      0           1           1        2
+        Subject6      0           0           1        1
+        Subject7      0           0           1        1
+        Subject8      0           0           1        1
+        Subject9      0           0           0        0
+        Subject10     0           0           0        0
+        --------------------------------------------------
+        Total         2           5           8       15
+        (sum_column)
+        """
+        sum_row = []
+        sum_column = [0 for i in range(len(data[0]))]
+        
+        for i in range(len(data)):
+            sum_row.append(sum(data[i]))
+            for j in range(len(data[i])):
+                sum_column[j] += data[i][j]
+        k = len(sum_column)
+
+        if 1 == 0:
+            # TODO: check data length
+            sys.exit()
+        else:
+            q_tmp1 = 0.0
+            q_tmp2 = 0.0
+
+            for i in range(len(sum_column)):
+                q_tmp1 += sum_column[i] * sum_column[i]
+                q_tmp2 += sum_column[i]
+                
+            q = (k - 1.0) * abs(k * q_tmp1 - q_tmp2 * q_tmp2)
+
+            q_tmp3 = 0.0
+            for j in range(len(sum_row)):
+                q_tmp3 += sum_row[j] * sum_row[j]
+
+            q /= k * q_tmp2 - q_tmp3
+
+            df = k - 1.0
+            p = 1.0 - stats.chi2.cdf(q, df)
+            print "q: " + str(q)
+            print "df: " + str(df)
+            print "p: " + str(p)
+            return p
--- statistics/src/sample_multiple_sample_test_of_nominal_scale.py
+++ statistics/src/sample_multiple_sample_test_of_nominal_scale.py
@@ -3,6 +3,7 @@
 import sys
 import numpy as np
 from unpaired_multiple_sample_test_of_nominal_scale import UnpairedMultipleSampleTestOfNominalScale
+from paired_multiple_sample_test_of_nominal_scale import PairedMultipleSampleTestOfNominalScale
 
 if __name__ == '__main__':
     args = sys.argv
@@ -10,8 +11,14 @@ if __name__ == '__main__':
         print "python sample_multiple_sample_test_of_nominal_scale.py <sample_type>"
         print "please choose sample type: "
         print "1: unpaired test"
+        print "2: paired test"
     else:
         if args[1] == "1":
             data = np.array([[12, 10, 8],[5, 5, 20], [7, 6, 7]])
             unpaired_multiple_sample_test_of_nominal_scale = UnpairedMultipleSampleTestOfNominalScale()
             unpaired_multiple_sample_test_of_nominal_scale.test(data)
+            
+        elif args[1] == "2":
+            data = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 0, 1], [1, 0, 1], [0, 1, 1], [0, 1, 1], [0, 1, 1], [0, 1, 1], [0, 1, 0], [0, 1, 0], [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]])
+            paired_multiple_sample_test_of_nominal_scale = PairedMultipleSampleTestOfNominalScale()
+            paired_multiple_sample_test_of_nominal_scale.test(data)
