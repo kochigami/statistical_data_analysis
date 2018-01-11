d589441b6f4c158264978cc0ebb8baeb8bd7f458
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2018-01-11 10:58:13 +0900

    modify variable name (horizontal, vertical are hard to understand)

--- statistics/src/unpaired_multiple_sample_test_of_nominal_scale.py
+++ statistics/src/unpaired_multiple_sample_test_of_nominal_scale.py
@@ -12,22 +12,23 @@ class UnpairedMultipleSampleTestOfNominalScale:
         """
         data = [[a,b,c], [d,e,f], [g,h,i]]
         example
-                       Yes   No   Yes&No  Total
+                       Yes   No   Yes&No  Total (Sum row)
         ---------------------------------------
         Condition1     a     b      c     a+b+c
         Condition2     d     e      f     d+e+f
         Condition3     g     h      i     g+h+i
         ---------------------------------------
         Total        a+d+g  b+e+h   c+f+i (= a+b+c+d+e+f+g+h+i)
+        (Sum column)
         """
-        sum_vertical = []
-        sum_horizontal = [0 for i in range(len(data))]
+        sum_row = []
+        sum_column = [0 for i in range(len(data[0]))]
         
         for i in range(len(data)):
-            sum_vertical.append(sum(data[i]))
+            sum_row.append(sum(data[i]))
             for j in range(len(data[i])):
-                sum_horizontal[j] += data[i][j]
-        N = sum(sum_vertical)
+                sum_column[j] += data[i][j]
+        N = sum(sum_row)
 
         if 1 == 0:
             # TODO: check data length
@@ -36,10 +37,10 @@ class UnpairedMultipleSampleTestOfNominalScale:
             chi_squared = 0.0
             for i in range(len(data)):
                 for j in range(len(data[i])):
-                    chi_squared += (data[i][j] * data[i][j]) / float (sum_vertical[i] * sum_horizontal[j])  
+                    chi_squared += (data[i][j] * data[i][j]) / float (sum_row[i] * sum_column[j])  
             chi_squared -= 1.0
             chi_squared *= N
-            df = (len(sum_vertical) - 1.0) * (len(sum_horizontal) - 1.0)
+            df = (len(sum_row) - 1.0) * (len(sum_column) - 1.0)
             p = 1.0 - stats.chi2.cdf(chi_squared, df)
             print "chi_squared: " + str(chi_squared)
             print "df: " + str(df)
