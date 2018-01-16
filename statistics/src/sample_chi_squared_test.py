8aa085c90505065b798f16b9f1198de25afc3002
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2018-01-16 10:07:29 +0900

    print chi value

--- statistics/src/sample_chi_squared_test.py
+++ statistics/src/sample_chi_squared_test.py
@@ -15,30 +15,30 @@ if __name__ == '__main__':
     else:
         d = ChiSquaredTestDrawGraph()
         # Q2 children
-        data = np.array([[43, 28],[19, 52]])
+        #data = np.array([[43, 28],[19, 52]])
         # Q2 adults
         # data = np.array([[8, 12],[11, 9]])
 
         # enquette
         # q1. adults (29), children (27)
-        # data = np.array([[20, 9],[18, 9]])
+        #data = np.array([[20, 9],[18, 9]])
         # q1. female (30), male (26)
-        # data = np.array([[21, 9],[17, 9]])
+        #data = np.array([[21, 9],[17, 9]])
 
         # q2. adults (29), children (27)
-        # data = np.array([[7, 22],[16, 11]])
+        #data = np.array([[7, 22],[16, 11]])
         # q2. female (30), male (26)
-        # data = np.array([[15, 15],[8, 18]])
+        #data = np.array([[15, 15],[8, 18]])
 
         # q3. adults (29), children (27)
-        # data = np.array([[16, 13],[12, 15]])
+        #data = np.array([[16, 13],[12, 15]])
         # q3. female (30), male (26)        
-        # data = np.array([[16, 14],[12, 14]])
+        #data = np.array([[16, 14],[12, 14]])
 
         # q4. adults (29), children (27)
-        # data = np.array([[9, 20],[15, 12]])
+        #data = np.array([[9, 20],[15, 12]])
         # q4. female (30), male (26)        
-        # data = np.array([[13, 17],[11, 15]])
+        data = np.array([[13, 17],[11, 15]])
 
         # q1. adults-children
         # d.draw_graph(data, ["Adults", "Children"], "The ratio of people who want to use a robot which helps doing household chores", "condition", "percentage", tight_layout=True, mode="unpaired")
