7abbbf3a64bbfb2dd16f558d4bf63101b2354ece
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2018-01-25 13:13:13 +0900

    [one-way anova] modify implementation

--- statistics/src/one_way_anova.py
+++ statistics/src/one_way_anova.py
@@ -16,77 +16,79 @@ class OneWayAnova:
     data = {'Japanese': [65, 85, 75, 85, 75, 80, 90, 75, 85, 65, 75, 85, 80, 85, 90],
             'English':  [65, 70, 80, 75, 70, 60, 65, 70, 85, 60, 65, 75, 70, 80, 75],
             'French' :  [70, 65, 85, 80, 75, 65, 75, 60, 85, 65, 75, 70, 65, 80, 75]}
-    mode: string. between or within.
+    mode: string. CR or RB.
+    
+    CR: completely randomized design
+    RB: randomized block design
     '''
-    def one_way_anova(self, data, mode="one-factor-repeated", threshold=0.05, comparison_mode="holm"):
-        # if mode is within test, sample num should be same in each category
-        if mode == "one-factor-factorical":
+    def one_way_anova(self, data, mode="CR", threshold=0.05, comparison_mode="holm"):
+        # if mode is RB, sample num should be same in each category
+        if mode == "RB":
             for i in range(len(data.keys()) - 1):
                 if len(data[(data.keys())[i]]) != len(data[(data.keys())[i+1]]):
                     print "Be sure that sample num of each category is same."
                     sys.exit()
 
-        if mode == "one-factor-repeated":
+        if mode == "CR":
             """
+            completely randomized design (CR design)
+
                    | sum of squares |     dof     |    mean squares    |          F            |      
             ------------------------------------------------------------------------------------
             gunkan | ss_between     | between_dof | mean_square_between| ms_between/ ms_within |
             gunnai | ss_within      | within_dof  | mean_square_within |                       |
             -------|----------------------------------------------------------------------------
             total  | ss_b+ss_w      | b_dof+w_dof | 
+
+            p: the number of condition
+            n: the number of each data per condition
             """
+            p = len(data.keys())
+            N = 0.0
+            for i in range(len(data.keys())):
+                N += len(data[(data.keys())[i]])
             
-            # calculate total average
-            total_average = 0.0
-            total_sample_num = 0.0
+            G = 0.0
             for i in range(len(data.keys())):
-                total_sample_num += len(data[(data.keys())[i]])
-                for j in range(len(data[(data.keys())[i]])):
-                    total_average += (data[(data.keys())[i]])[j]
-            total_average = total_average / total_sample_num
+                G += sum(data[(data.keys())[i]])
 
-            # calculate average per category
-            average_per_group = []
-            for i in range(len(data.keys())):
-                average_per_group.append(np.mean(data[(data.keys())[i]]))
+            X = pow(G, 2.0) / float(N)
 
-            # calculate ss_total (sum of squares per total samples)
-            ss_total = 0.0
+            AS = 0.0
             for i in range(len(data.keys())):
                 for j in range(len(data[(data.keys())[i]])):
-                    ss_total += pow(((data[(data.keys())[i]])[j] - total_average), 2.0)
+                    AS += pow(data[(data.keys())[i]][j], 2.0)
 
-            # calculate ss_between (sum of squares per category)
-            ss_between = 0.0
+            A = 0.0
             for i in range(len(data.keys())):
-                ss_between += pow((average_per_group[i] - total_average), 2.0) * len(data[(data.keys())[i]])
+                A += pow(sum(data[(data.keys())[i]]), 2.0) / len(data[(data.keys())[i]])
 
-            # calculate ss_within (sum of squares per category)
-            ss_within = ss_total - ss_between
+            SSa = A - X
+            SSwc = AS - A
+            SSr = AS - X
 
             # calculate dof
-            between_dof = len(data.keys()) - 1
-            total_dof = total_sample_num - 1
+            between_dof = p - 1
+            total_dof = N - 1
             within_dof = total_dof - between_dof
 
-            # calculate mean square
-            mean_square_between = ss_between / float(between_dof)
-            mean_square_within = ss_within / float(within_dof)
-
-            # calculate F
-            F = mean_square_between / mean_square_within
+            MSa = SSa / between_dof
+            MSwc = SSwc / within_dof
+            F = MSa / MSwc
 
             # calculate p
             p = calc_f.sf(F, between_dof, within_dof)
 
-            answer_list = [[math.ceil(ss_between * 100.0) * 0.01, int(between_dof), math.ceil(mean_square_between * 100.0) * 0.01, math.ceil(F * 100.0) * 0.01, math.ceil(p * 1000.0) * 0.001],
-                           [math.ceil(ss_within * 100.0) * 0.01, int(within_dof), math.ceil(mean_square_within * 100.0) * 0.01, '--', '--'], 
-                           [math.ceil((ss_between + ss_within) * 100.0) * 0.01, int(between_dof + within_dof),'--', '--', '--']]
-            self.comparison(data, mean_square_between, between_dof, threshold, comparison_mode)
+            answer_list = [[math.ceil(SSa * 100.0) * 0.01, int(between_dof), math.ceil(MSa * 100.0) * 0.01, math.ceil(F * 100.0) * 0.01, math.ceil(p * 1000.0) * 0.001],
+                           [math.ceil(SSwc * 100.0) * 0.01, int(within_dof), math.ceil(MSwc * 100.0) * 0.01, '--', '--'], 
+                           [math.ceil((SSr) * 100.0) * 0.01, int(between_dof + within_dof),'--', '--', '--']]
+            self.comparison(data, MSwc, between_dof, threshold, comparison_mode)
             return answer_list
 
-        elif mode == "one-factor-factorical":
-            """
+        elif mode == "RB":
+            """            
+            randomized block (RB design)
+
                    | sum of squares |   dof       |     mean squares    |                   F                    |       
             ------------------------------------------------------------------------------------------------------
             youin  | ss_between     | between_dof | mean_square_between | mean_square_between/ mean_square_error |
@@ -96,74 +98,63 @@ class OneWayAnova:
             Total  | ss_b+s+e       | b+s+e_dof   | 
             """
             
-            # calculate total
-            total_S = 0.0
-            total_sample_num = 0.0
-            for i in range(len(data.keys())):
-                total_sample_num += len(data[(data.keys())[i]])
-                for j in range(len(data[(data.keys())[i]])):
-                    total_S += pow((data[(data.keys())[i]])[j], 2.0)
-
-            # each category
-            category_S = 0.0
-            for i in range(len(data.keys())):
-                category_S += pow(sum(data[(data.keys())[i]]), 2.0) / len(data[(data.keys())[i]])
+            p = len(data.keys())
+            n = len(data[(data.keys())[0]])
             
-            # each subject
-            # sample num should be same in each category
-            # that's why checking array length at first code
-            subject_S = 0.0
-            subject_S_tmp = 0.0
-            target_num = 0
-            count = 0
-            for i in range(len(data[(data.keys())[0]])):
-                for j in range(len(data.keys())):
-                    if i == target_num:
-                        subject_S_tmp += (data[(data.keys())[j]])[i]
-                        count += 1
-                    if count == len(data.keys()):
-                        subject_S += pow(subject_S_tmp, 2.0) / len(data.keys())
-                        count = 0
-                        target_num += 1
-                        subject_S_tmp = 0.0
-
-            # calculate x (correction term)
-            x = 0.0
+            G = 0.0
             for i in range(len(data.keys())):
-                x += sum(data[(data.keys())[i]])
-            x = pow(x, 2.0) / (len(data.keys()) * len(data[(data.keys())[0]]))
+                G += sum(data[(data.keys())[i]])
 
-            # calculate sum of squares
-            ss_total = total_S - x
-            ss_between = category_S - x
-            ss_subject = subject_S - x
-            ss_error = ss_total - ss_between - ss_subject
+            X = pow(G, 2.0) / (float(p) * n)
+
+            AS = 0.0
+            for i in range(len(data.keys())):
+                for j in range(len(data[(data.keys())[i]])):
+                    AS += pow(data[(data.keys())[i]][j], 2.0)
+
+            A = 0.0
+            for i in range(len(data.keys())):
+                A += pow(sum(data[(data.keys())[i]]), 2.0) / len(data[(data.keys())[i]])
+            print A
+
+            S = 0.0
+            for i in range(len(data[(data.keys())[0]])):
+                tmp = 0.0
+                for j in range(len(data.keys())):
+                    tmp += data[(data.keys())[j]][i]
+                S += pow(tmp, 2.0) / p
+
+            print S
+
+            SSs = S - X
+            SSa = A - X
+            SSres = AS - A - S + X
+            SSr = AS - X
 
             # calculate dof (group type & sample number of each type)
-            between_dof = len(data.keys()) - 1.0
-            subject_dof = len(data[(data.keys())[0]]) - 1.0
-            error_dof =  total_sample_num - 1 - between_dof - subject_dof
+            between_dof = p - 1.0
+            subject_dof = n - 1.0
+            error_dof =  n * p - 1 - between_dof - subject_dof
 
-            # calculate mean square
-            mean_square_between = ss_between / between_dof
-            mean_square_subject = ss_subject / subject_dof            
-            mean_square_error = ss_error / error_dof
+            MSa = SSa / between_dof
+            MSs = SSs / subject_dof
+            MSres = SSres / error_dof 
+            Fa = MSa / MSres
+            Fs = MSs / MSres
 
-            # calculate F
-            F = mean_square_between / mean_square_error
-
-            # calculate p
-            p = calc_f.sf(F, between_dof, error_dof)
+            pa = calc_f.sf(Fa, between_dof, error_dof)
+            ps = calc_f.sf(Fs, subject_dof, error_dof)
  
-            answer_list = [[math.ceil(ss_between * 100.0) * 0.01, int(between_dof), math.ceil(mean_square_between * 100.0) * 0.01, math.ceil(F * 100.0) * 0.01, math.ceil(p * 1000.0) * 0.001],
-                           [math.ceil(ss_subject * 100.0) * 0.01, int(subject_dof), math.ceil(mean_square_subject * 100.0) * 0.01, '--', '--'],
-                           [math.ceil(ss_error * 100.0) * 0.01, int(error_dof), math.ceil(mean_square_error * 100.0) * 0.01, '--', '--'],
-                           [math.ceil(ss_total * 100.0) * 0.01, int(between_dof + subject_dof + error_dof),'--', '--', '--']]
-            self.comparison(data, mean_square_between, between_dof, threshold, comparison_mode)
+            answer_list = [[math.ceil(SSa *100.0) *0.01, int(between_dof), math.ceil(MSa *100.0) *0.01, math.ceil(Fa *100.0) *0.01, math.ceil(pa *1000.0) *0.001],
+                           [math.ceil(SSs *100.0) *0.01, int(subject_dof), math.ceil(MSs *100.0) *0.01, math.ceil(Fs *100.0) *0.01, math.ceil(ps *1000.0) *0.001],
+                           [math.ceil(SSres *100.0) *0.01, int(error_dof), math.ceil(MSres *100.0) *0.01, '--', '--'],
+                           [math.ceil((SSa + SSs + SSres) *100.0) *0.01, int(between_dof + subject_dof + error_dof),'--', '--', '--']]
+            self.comparison(data, SSa, between_dof, threshold, comparison_mode)
+            self.comparison(data, SSs, subject_dof, threshold, comparison_mode)
             return answer_list
 
         else:
-            print "Please choose mode 'one-factor-repeated' or 'one-factor-factorical'."
+            print "Please choose mode 'CR' or 'RB'."
             return False
 
     def comparison(self, data, mean_square_between, between_dof, threshold=0.05, mode="holm"):
