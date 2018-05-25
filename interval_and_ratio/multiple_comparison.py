7c7839b4cb7e5ab36479218e52138d632552e962
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2018-05-25 12:15:29 +0900

    add comments to multiple comparison

--- interval_and_ratio/multiple_comparison.py
+++ interval_and_ratio/multiple_comparison.py
@@ -12,32 +12,60 @@ from ordinal.unpaired_two_sample_test_of_ordinal_scale import UnpairedTwoSampleT
 class MultipleComparison:
     def test(self, data, test="none", alpha=0.05):
         '''
-        data: {A: [1,2,3], B: [2,4,6], ...} 
-        
+        ryan method
+
+        data: {A: [1,2,3], B: [2,4,6], ...}
+
         alpha_dash = 2 * alpha / (m * ( r - 1 ))
         m: the number of step 
-        r: the total number of step
+        r: the number of step - the total number of step
         '''
         m = len(data.keys())
+        '''
+        calculate
+        data_average: {A: 2, B: 4, ...}
+        '''
         data_average = {}
-        data_new = OrderedDict()
         for r in range(m):
-            data_average[(data.keys())[r]] = np.average(data[(data.keys())[r]])        
+            data_average[(data.keys())[r]] = np.average(data[(data.keys())[r]])
+        '''
+        sort data_average
+        set a result as data_tmp
+        '''
         data_tmp = sorted(data_average.items(), key=operator.itemgetter(1))
+        '''
+        make OrderedDict "data_new" by copying data_tmp
+        '''
+        data_new = OrderedDict()
         for r in range(len(data_tmp)):
             data_new[(data_tmp[r])[0]] = (data_tmp[r])[1]
         
+        '''
+        r: the number of step - the total number of step
+        '''
         for i in range(m):
             r = m - i
             if r > 1:
-                print "r: " + str(r)
+                print "\n[Step {}]".format(i+1)
+                '''
+                show alpha_dash as threshold of p
+                '''
+                print "threshold is: {}".format(2.0 * alpha / (m * ( r - 1 )))
                 for j in range(0, i+1):
-                    # just print paired sample
-                    # need to run test and get p value
+                    '''
+                    create data of a chosen pair
+                    '''
                     test_data = OrderedDict()
                     test_data[(data_new.keys())[j]] = data[(data_new.keys())[j]]
                     test_data[(data_new.keys())[j+r-1]] = data[(data_new.keys())[j+r-1]]
-                    print "comparison of " + str((data_new.keys())[j]) + " and " + str((data_new.keys())[j+r-1])
+                    '''
+                    print paired sample
+                    '''
+                    print "\ncomparison of {} and {}".format((data_new.keys())[j], (data_new.keys())[j+r-1])
+                    '''
+                    run test and get p value
+                    '''
+                    # FIXME: add explanation about what test variable means
                     if test == "chi-squared":
                         # fisher's exact test and chi-squared test
                         unpaired_two_sample_test_of_nominal_scale = UnpairedTwoSampleTestOfNominalScale()
@@ -55,5 +83,3 @@ class MultipleComparison:
                         paired_two_sample_test_of_ordinal_scale.test(test_data, mode="signed_test")
                     else:
                         print "Please input test name"
-                print "threshold is: " + str(2.0 * alpha / (m * ( r - 1 ))) # alpha_dash
-                print "\n"
