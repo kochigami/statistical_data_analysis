a03fa0277b212ea7d3e30d979c7fde65a5fd113b
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2018-06-01 12:29:58 +0900

    plot number of sample in box plot graph

--- draw/draw_box_graph.py
+++ draw/draw_box_graph.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 class DrawBoxGraph:
     def draw_graph(self, data, title, xlabel, ylabel):
@@ -13,11 +14,29 @@ class DrawBoxGraph:
             tmp_data.append(i)
             
         bp = ax.boxplot(tmp_data)
-        
+
         tmp_label = []
         for i in data.keys():
             tmp_label.append(i)
         ax.set_xticklabels(tmp_label)
+
+        '''
+        plot number of sample for each condition as text
+        '''
+        # calculate medians for getting y position
+        medians = []
+        for i in range(len(tmp_data)):
+            medians.append(np.median(tmp_data[i]))
+        # set plotting text (number of sample for each condition)
+        nobs = []
+        for i in range(len(tmp_data)):
+            nobs.append(len(tmp_data[i]))
+        nobs = ["n: " + str(i) for i in nobs]
+        # plot nobs (x position is 1, 2, 3,...)
+        pos = range(len(nobs))
+        for tick in pos:
+            # x position of the text, y position of the text, text, other options
+            plt.text(pos[tick]+1, medians[tick] * 1.01, nobs[tick], horizontalalignment='center', size='medium', color='black', weight='semibold')
 
         plt.title(title)
         plt.xlabel(xlabel)
