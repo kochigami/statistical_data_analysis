7172746e9955bdbcbdd59400fb60cc83e6e8cd36
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2018-06-13 11:18:20 +0900

    refactor t-test

--- interval_and_ratio/paired_two_sample_test_of_interval_and_ratio_scale.py
+++ interval_and_ratio/paired_two_sample_test_of_interval_and_ratio_scale.py
@@ -1,37 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from scipy import stats
-import numpy as np
-import math
-from scipy.stats import t as calc_p
-# referenced as calc_p because of the error below:
-# File "/home/kochigami/my_tutorial/statistics/src/t_test/t_test.py", line 80, in unpaired_ttest
-# p = t.sf(t_value, dof)
-# UnboundLocalError: local variable 't' referenced before assignment
-# t test
+import sys
 
 class PairedTwoSampleTestOfIntervalAndRatioScale:
     def test(self, data):
-        """
+        '''
         data = {'Japanese':  [68, 75, 80, 71, 73, 79, 69, 65],
-                'English': [80, 76, 84, 93, 76, 80, 79, 84]}
-        """
+                'English':   [80, 76, 84, 93, 76, 80, 79, 84]}
+        '''
 
-        """
+        '''
         If samples are paired,
         we use paired t-test
-        I followed this website for calculation: 
+        reference: 
         student's test: http://kogolab.chillout.jp/elearn/hamburger/chap5/sec3.html
-        """
+        scipy documentation: https://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.stats.ttest_rel.html
+        '''
         # calculate t & p value
         x = data[(data.keys())[0]]
         y = data[(data.keys())[1]]
         if len(x) == len(y):
-            t, p = stats.ttest_rel(x, y)
-            print ("dof = " + str(len(x) - 1))
-            print ("t value = %(t)s" %locals() )
-            print ("p value = %(p)s" %locals() )
-            return p
+            t_value, p_value = stats.ttest_rel(x, y)
+            print ("dof = {}".format(len(x) - 1))
+            print ("t value = {}".format(t_value))
+            print ("p value = {}".format(p_value))
+            return p_value
         else:
             print "Please make sure the sample num is same in two conditions."
-            return False
+            sys.exit()
