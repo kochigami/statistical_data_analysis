a1826dacec4726992cb35eaf17a70f923ed57fa1
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2018-07-03 11:25:53 +0900

    refactor SPF

--- interval_and_ratio/SPFpq.py
+++ interval_and_ratio/SPFpq.py
@@ -68,22 +68,37 @@ class SPF_pq:
                     n_j.append(len(data[i]))
                     break
 
-        # Sij: 
+        # Sij: total sum of data per subject
+        # ex. [18, 19, 19, 19, 29, 10, 16, 11, 20, 21]
         Sij = []
         tmp = [[] for i in range(len(n_j))]
         for i in range(len(label_A)):
             tmp[i] = [0 for s in range(n_j[i])]
+            # tmp = [[0 0 0 0 0], [0 0 0 0]] 
             for j in range(n_j[i]):
                 for k in range(len(data.keys())):
                     if label_A[i] in (data.keys())[k]:
                         tmp[i][j] += data[(data.keys())[k]][j]
                 Sij.append(tmp[i][j])
 
+        # AS: sum of Sij^2 / q
         AS = 0.0
         for i in range(len(Sij)):
             AS += pow(Sij[i], 2.0) / q
 
+        # A_sum: category1 sum
+        A_sum = []
+        A_sum_tmp = 0.0
+        for i in range(len(label_A)):
+            for j in range(len(data.keys())):
+                if label_A[i] in (data.keys())[j]:
+                    A_sum_tmp += sum(data[(data.keys())[j]])
+            A_sum.append(A_sum_tmp)
+            A_sum_tmp = 0.0
+
+
         if mode == "equal":
+            # G: sum of all the data
             G = 0.0
             for i in range(len(data.keys())):
                 for j in range(len(data[(data.keys()[i])])):
@@ -95,21 +110,12 @@ class SPF_pq:
 
             X = pow(G, 2.0) / (n * p * q)
 
-            A_sum = []
-            A_sum_tmp = 0.0
-            # category1 sum
-            for i in range(len(label_A)):
-                for j in range(len(data.keys())):
-                    if label_A[i] in (data.keys())[j]:
-                        A_sum_tmp += sum(data[(data.keys())[j]])
-                A_sum.append(A_sum_tmp)
-                A_sum_tmp = 0.0
-
+            # A: sum of Aj^2/ n*q
             A = 0.0
             for i in range(len(A_sum)):
                 A += pow(A_sum[i], 2.0) / (n * q)
 
-            # category2 sum
+            # B_sum: category2 sum
             B_sum = []
             B_sum_tmp = 0.0
             for i in range(len(label_B)):
@@ -119,21 +125,21 @@ class SPF_pq:
                 B_sum.append(B_sum_tmp)
                 B_sum_tmp = 0.0
 
+            # B: sum of Bk^2 / n*p
             B = 0.0
             for i in range(len(B_sum)):
                 B += pow(B_sum[i], 2.0) / (n * p)
-            # same as CRFpq & size is equal #
-                                
+                
+            # calculate SS
             # same as CRFpq & size is equal #
             SSa = A - X
             SSb = B - X
             SSaxb = AB - A - B + X
             SSt = ABS - X
-            # same as CRFpq & size is equal #
-                
+            
+            # These are new for SPF design
             SSsa = AS - A
             SSbxsa = ABS - AB - AS + A
-            
             SA_dof = p * (n - 1)
             BxSA_dof = p * (q - 1) * (n - 1)
 
@@ -141,10 +147,12 @@ class SPF_pq:
             MSa = SSa / A_dof
             MSb = SSb / B_dof
             MSaxb = SSaxb / AxB_dof
-            # same as CRFpq & size is equal #
+             
+            # These are new for SPF design
             MSsa = SSsa / SA_dof
             MSbxsa = SSbxsa / BxSA_dof
 
+            # calculate F
             Fa = MSa / MSsa
             Fb = MSb / MSbxsa
             Faxb = MSaxb / MSbxsa
@@ -153,30 +161,21 @@ class SPF_pq:
             p_1 = calc_f.sf(Fa, A_dof, BxSA_dof)
             p_2 = calc_f.sf(Fb, B_dof, BxSA_dof)
             p_1x2 = calc_f.sf(Faxb, AxB_dof, BxSA_dof)
-            
-        else:
-            A_sum_tmp = 0.0
-            A_sum = []
-            for j in range(len(label_A)):
-                for i in range(len(data.keys())):
-                    if label_A[j] in (data.keys())[i]:
-                        A_sum_tmp += sum(data[(data.keys())[i]])
-                A_sum.append(A_sum_tmp)
-                A_sum_tmp = 0.0
 
+        else:
+            # A: Aj^2 / nj * q
             A = 0.0
             for i in range(len(A_sum)):
                 A += pow(A_sum[i], 2.0) / (n_j[i] * q)
 
-            # same 
-            G_dash = 0.0
+            # unweighted_mean
             unweighted_mean = []
             for i in range(len(data.keys())):
                 unweighted_mean.append(sum(data[(data.keys())[i]]) / float(len(data[(data.keys())[i]])))
 
             A_sum_tmp = 0.0
-            A_sum = []
             A_num_tmp = 0.0
+            A_sum = []
             for j in range(len(label_A)):
                 for i in range(len(data.keys())):
                     if label_A[j] in (data.keys())[i]:
@@ -194,38 +193,44 @@ class SPF_pq:
                 for i in range(len(data.keys())):
                     if label_B[j] in (data.keys())[i]:
                         B_sum_tmp += unweighted_mean[i]
-                        B_num_tmp += 1
+                        B_num_tmp += 1.0
                 B_sum_tmp = B_sum_tmp / B_num_tmp
                 B_sum.append(B_sum_tmp)
                 B_sum_tmp = 0.0
                 B_num_tmp = 0.0
 
+            # G_dash: sum of non-weighted average per condition
             G_dash = sum(unweighted_mean)
+
+            # X_dash: G_dash^2 / p * q
             X_dash = pow(G_dash, 2.0) / (p * q)
 
+            # A_dash
             A_dash = 0.0
             for i in range(len(A_sum)):
                 A_dash += pow(A_sum[i], 2.0)
             A_dash *= q
 
+            # B_dash
             B_dash = 0.0
             for i in range(len(B_sum)):
                 B_dash += pow(B_sum[i], 2.0)
             B_dash *= p
-                
+        
+            # AB_dash
             AB_dash = 0.0
             for i in range(len(unweighted_mean)):
                 AB_dash += pow(unweighted_mean[i], 2.0)
-            # same
+            
+            # n_tilde
             tmp = 0.0
             for i in range(len(n_j)):
                 tmp += 1.0 / n_j[i]
             n_tilde = p / tmp
 
-            # same
+            # calculate SS
             SSsa = AS - A
             SSbxsa = ABS - AB - AS + A
-            # same
             SSa = n_tilde * (A_dash - X_dash)
             SSb = n_tilde * (B_dash - X_dash)
             SSaxb = n_tilde * (AB_dash - A_dash - B_dash + X_dash)
@@ -234,12 +239,14 @@ class SPF_pq:
             SA_dof = N - p
             BxSA_dof = (N - p) * (q - 1)
 
+            # calculate MS
             MSsa = SSsa / SA_dof
             MSa = SSa / A_dof
             MSb = SSb / B_dof
             MSaxb = SSaxb / AxB_dof
             MSbxsa = SSbxsa / BxSA_dof
 
+            # calculate F
             Fa = MSa / MSsa
             Fb = MSb / MSbxsa
             Faxb = MSaxb / MSbxsa
