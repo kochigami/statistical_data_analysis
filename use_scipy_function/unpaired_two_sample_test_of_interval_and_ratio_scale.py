f2fb3cfb66fd999510054e27ed2bde9ca273a709
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2018-11-20 11:28:54 +0900

    [interval, two-sample, unpaired]

---
+++ use_scipy_function/unpaired_two_sample_test_of_interval_and_ratio_scale.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from scipy import stats
+import numpy as np
+import math
+
+class UnpairedTwoSampleTestOfIntervalAndRatioScale:
+    def test(self, data):
+        '''
+        data = {'Japanese':  [68, 75, 80, 71, 73, 79, 69, 65],
+                'English': [80, 76, 84, 93, 76, 80, 79, 84]}
+
+        If samples are not paired, we use unpaired t-test.
+        Currently, we don't care whether two samples are considered to have equal population variance.
+        reference: https://bellcurve.jp/statistics/course/9446.html
+        '''
+
+        x = data[(data.keys())[0]]
+        y = data[(data.keys())[1]]
+        nx = len(data[(data.keys())[0]])
+        ny = len(data[(data.keys())[1]])
+        dof = nx + ny - 2.0
+        
+        # welch's test
+        t, p = stats.ttest_ind(x, y, equal_var=False)
+
+        
+        ### heteroscedasticity: hi tou bunsan
+        print ("t-test with heteroscedasticity")
+
+        print("dof = {}".format(dof))
+        print("t value = {}".format(t))
+        print("p value = {}".format(p))
+        return p
